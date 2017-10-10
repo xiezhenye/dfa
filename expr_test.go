115b12eb6fee11c1ceaf6a489a1b446d308c05bc
xiezhenye <xiezhenye@gmail.com>
2017-10-10 15:43:37 +0800

    add test

--- expr_test.go
+++ expr_test.go
@@ -1,288 +1,356 @@
 package dfa
 
-import (
-	"fmt"
-	"testing"
+import "testing"
 
-	"h12.me/gspec"
-)
-
-var (
-	s        = Str
-	c        = Char
-	b        = Between
-	con      = Con
-	or       = Or
-	and      = And
-	optional = Optional
-	graphOpt = &GraphOption{"Ubuntu Mono", true}
-)
-
-const (
-	hexLabel = iota
-	decimalLabel
-	identLabel
-)
-
-var threeToken = func() *M {
-	decimalDigit := b('0', '9')
-	hexDigit := or(b('0', '9'), b('a', 'f'), b('A', 'F'))
-	letter := or(b('a', 'z'), b('A', 'Z'))
-
-	hexLit := con(s(`0`), c(`xX`), hexDigit.AtLeast(1)).As(hexLabel)
-	decimalLit := decimalDigit.AtLeast(1).As(decimalLabel)
-	ident := con(letter, or(letter, decimalDigit).Repeat()).As(identLabel)
-
-	return or(hexLit, decimalLit, ident).Minimize()
-}
-
-var bsas = func() *M {
-	bsa := con(s("b").Repeat(), s("a"))
-	return con(bsa, bsa).Repeat()
-}
-
-var asbs = func() *M {
-	asb := con(s("a").Repeat(), s("b"))
-	return con(asb, asb).Repeat()
-}
-
-func TestExpr(t *testing.T) {
-	expect := gspec.Expect(t.FailNow)
-	for i, testcase := range []struct {
-		m *M
-		s string
-	}{
-		{
-			s(""), `
-			s0$`,
-		},
-		{
-			s("a"), `
-			s0
-				'a'     s1
-			s1$`,
-		},
-		{
-			s("ab"), `
-			s0
-				'a'     s1
-			s1
-				'b'     s2
-			s2$`,
-		},
-		{
-			c("abc"), `
-			s0
-				'a'-'c' s1
-			s1$`,
-		},
-		{
-			b('a', 'c'),
-			c("abc").Minimize().dump(),
-		},
-		{
-			con(s("a"), s("b")),
-			s("ab").dump(),
-		},
-		{
-			or(s("a"), s("b")),
-			c("ab").Minimize().dump(),
-		},
-		{
-			s("a").Repeat(), `
-			s0$
-				'a'     s0`,
-		},
-		{
-			s("ab").Repeat(), `
-			s0$
-				'a'     s1
-			s1
-				'b'     s0`,
-		},
-		{
-			s("ab").AtLeast(1), `
-			s0
-				'a'     s1
-			s1
-				'b'     s2
-			s2$
-				'a'     s1`,
-		},
-		{
-			con(s(`a`), optional(s(`b`))), `
-			s0
-				'a'     s1
-			s1$
-				'b'     s2
-			s2$`,
-		},
-		{
-			con(s(`a`), s(`b`).Repeat()), `
-			s0
-				'a'     s1
-			s1$
-				'b'     s1`,
-		},
-		{
-			s(`a`).Repeat(3),
-			s(`aaa`).dump(),
-		},
-		{
-			threeToken(), `
-			s0
-				'0'     s1
-				'1'-'9' s2
-				'A'-'Z' s3
-				'a'-'z' s3
-			s1$3
-				'0'-'9' s2
-				'X'     s4
-				'x'     s4
-			s2$3
-				'0'-'9' s2
-			s3$4
-				'0'-'9' s3
-				'A'-'Z' s3
-				'a'-'z' s3
-			s4
-				'0'-'9' s5
-				'A'-'F' s5
-				'a'-'f' s5
-			s5$2
-				'0'-'9' s5
-				'A'-'F' s5
-				'a'-'f' s5`,
-		},
-		{
-			b(0, '\U0010ffff'), `
-			s0
-				00-7f   s1
-				c2-df   s2
-				e0      s3
-				e1-ef   s4
-				f0      s5
-				f1-f3   s6
-				f4      s7
-			s1$
-			s2
-				80-bf   s1
-			s3
-				a0-bf   s2
-			s4
-				80-bf   s2
-			s5
-				90-bf   s4
-			s6
-				80-bf   s4
-			s7
-				80-8f   s4`,
-		},
-		{
-			b(0x3bf, 0x3c0), `
-			s0
-				ce      s1
-				cf      s2
-			s1
-				bf      s3
-			s2
-				80      s3
-			s3$`,
-		},
-		{
-			bsas(), `
-			s0$
-				'a'     s1
-				'b'     s0
-			s1
-				'a'     s0
-				'b'     s1`,
-		},
-		{
-			bsas().Complement(), `
-			s0
-				'a'     s1
-				'b'     s0
-			s1$
-				'a'     s0
-				'b'     s1`,
-		},
-		{
-			or(bsas(), asbs().Complement()), `
-			s0$
-				'a'     s1
-				'b'     s2
-			s1
-				'a'     s0
-				'b'     s3
-			s2$
-				'a'     s3
-				'b'     s0
-			s3$
-				'a'     s2
-				'b'     s1`,
-		},
-		{
-			and(bsas(), asbs().Complement()), `
-			s0
-				'a'     s1
-				'b'     s2
-			s1
-				'a'     s0
-				'b'     s3
-			s2$
-				'a'     s3
-				'b'     s0
-			s3
-				'a'     s2
-				'b'     s1`,
-		},
-		{
-			bsas().Exclude(asbs().Complement()), `
-			s0$
-				'a'     s1
-				'b'     s2
-			s1
-				'a'     s0
-				'b'     s3
-			s2
-				'a'     s3
-				'b'     s0
-			s3
-				'a'     s2
-				'b'     s1`,
-		},
-		{
-			c("ab").Exclude(c("a")), `
-			s0
-				'b'     s1
-			s1$`,
-		},
-		{
-			con(b(0xFEFE, 0xFF00).Exclude("\uFEFF").AtLeast(1), "\uFEFF"), `
-			s0
-				ef      s1
-			s1
-				bb      s2
-				bc      s3
-			s2
-				be      s4
-			s3
-				80      s4
-			s4
-				ef      s5
-			s5
-				bb      s6
-				bc      s3
-			s6
-				be      s4
-				bf      s7
-			s7$`,
-		},
-	} {
-		expect(fmt.Sprintf("dump of test case %d", i), testcase.m.Minimize().dump()).Equal(gspec.Unindent(testcase.s))
+func TestStr(t *testing.T) {
+	s := Str("hello").As(100)
+	n, l, ok := s.Match([]byte("hello"))
+	if !ok || n != 5 || l != 100 {
+		t.Error("match fail")
+	}
+	n, l, ok = s.Match([]byte("hello world"))
+	if !ok || n != 5 || l != 100 {
+		t.Error("match fail")
+	}
+	n, l, ok = s.Match([]byte("!hello"))
+	if ok {
+		t.Error("match should fail")
 	}
 }
 
-func TestSingle(t *testing.T) {
+func TestChar(t *testing.T) {
+	c := Char("+-*/").As(101)
+	n, l, ok := c.Match([]byte("+"))
+	if !ok || n != 1 || l != 101 {
+		t.Error("match fail")
+	}
+	n, l, ok = c.Match([]byte("*1"))
+	if !ok || n != 1 || l != 101 {
+		t.Error("match fail")
+	}
+	n, l, ok = c.Match([]byte("1*"))
+	if ok {
+		t.Error("match should fail")
+	}
+}
+
+func TestBetween(t *testing.T) {
+	a := Between(rune('a'), rune('z')).As(101)
+	n, l, ok := a.Match([]byte("a+b"))
+	if !ok || n != 1 || l != 101 {
+		t.Error("match fail")
+	}
+	n, l, ok = a.Match([]byte("111"))
+	if ok {
+		t.Error("match should fail")
+	}
+	a = Between(rune(0x00), rune(0x20)).As(101)
+	n, l, ok = a.Match([]byte(" "))
+	if !ok || n != 1 || l != 101 {
+		t.Error("match fail")
+	}
+	a = Between(rune('一'), rune('龥')).As(102)
+	n, l, ok = a.Match([]byte("中"))
+	if !ok || n != 3 || l != 102 {
+		t.Error("match fail")
+	}
+}
+
+func TestBetweenByte(t *testing.T) {
+	a := BetweenByte('a', 'z').As(101)
+	n, l, ok := a.Match([]byte("a+b"))
+	if !ok || n != 1 || l != 101 {
+		t.Error("match fail")
+	}
+	n, l, ok = a.Match([]byte("111"))
+	if ok {
+		t.Error("match should fail")
+	}
+	a = BetweenByte('\x00', '\x20').As(101)
+	n, l, ok = a.Match([]byte(" "))
+	if !ok || n != 1 || l != 101 {
+		t.Error("match fail")
+	}
+}
+
+func TestCharClass(t *testing.T) {
+	a := CharClass("Han").As(102)
+	n, l, ok := a.Match([]byte("中"))
+	if !ok || n != 3 || l != 102 {
+		t.Error("match fail")
+	}
+	n, l, ok = a.Match([]byte("english"))
+	if ok {
+		t.Error("match should fail")
+	}
+}
+
+func TestAnd(t *testing.T) {
+	a := BetweenByte('a', 'e')
+	b := BetweenByte('c', 'g')
+	c := And(a, b)
+	n, _, ok := c.Match([]byte("cc"))
+	if !ok || n != 1 {
+		t.Error("match fail")
+	}
+	n, _, ok = c.Match([]byte("aa"))
+	if ok {
+		t.Error("match should fail")
+	}
+	n, _, ok = c.Match([]byte("ff"))
+	if ok {
+		t.Error("match should fail")
+	}
+}
+
+func TestCon(t *testing.T) {
+	a := BetweenByte('a', 'z')
+	b := BetweenByte('0', '9')
+	c := Con(a, b)
+	n, _, ok := c.Match([]byte("a1"))
+	if !ok || n != 2 {
+		t.Error("match fail")
+	}
+	n, _, ok = c.Match([]byte("aa"))
+	if ok {
+		t.Error("match should fail")
+	}
+	n, _, ok = c.Match([]byte("11"))
+	if ok {
+		t.Error("match should fail")
+	}
+}
+
+func TestOr(t *testing.T) {
+	ae := BetweenByte('a', 'e')
+	cg := BetweenByte('c', 'g')
+	a := Or(ae, cg)
+	n, _, ok := a.Match([]byte("cat"))
+	if !ok || n != 1 {
+		t.Error("match fail")
+	}
+	n, _, ok = a.Match([]byte("egg"))
+	if !ok || n != 1 {
+		t.Error("match fail")
+	}
+	n, _, ok = a.Match([]byte("bad"))
+	if !ok || n != 1 {
+		t.Error("match fail")
+	}
+	n, _, ok = a.Match([]byte("fat"))
+	if !ok || n != 1 {
+		t.Error("match fail")
+	}
+	n, _, ok = a.Match([]byte("add"))
+	if !ok || n != 1 {
+		t.Error("match fail")
+	}
+	n, _, ok = a.Match([]byte("get"))
+	if !ok || n != 1 {
+		t.Error("match fail")
+	}
+	n, _, ok = a.Match([]byte("123"))
+	if ok {
+		t.Error("match should fail")
+	}
+	n, _, ok = a.Match([]byte("hat"))
+	if ok {
+		t.Error("match should fail")
+	}
+}
+
+func TestOr2(t *testing.T) {
+	nm := BetweenByte('a', 'e').AtLeast(1).As(100)
+	kw := Str("hello").As(200)
+	a := Or(nm, kw)
+	n, l, ok := a.Match([]byte("abc"))
+	if !ok || n != 3 || l != 100 {
+		t.Error("match fail")
+	}
+	n, l, ok = a.Match([]byte("hello"))
+	if !ok || n != 5 || l != 200 {
+		t.Error("match fail")
+	}
+}
+
+func TestAtLeast(t *testing.T) {
+	a := BetweenByte('a', 'z').AtLeast(3)
+	n, _, ok := a.Match([]byte("add"))
+	if !ok || n != 3 {
+		t.Error("match fail")
+	}
+	n, _, ok = a.Match([]byte("flag"))
+	if !ok || n != 4 {
+		t.Error("match fail")
+	}
+	n, _, ok = a.Match([]byte("xx"))
+	if ok {
+		t.Error("match should fail")
+	}
+	n, _, ok = a.Match([]byte("123"))
+	if ok {
+		t.Error("match should fail")
+	}
+	a = BetweenByte('a', 'z').AtLeast(0)
+	n, _, ok = a.Match([]byte("add"))
+	if !ok || n != 3 {
+		t.Error("match fail")
+	}
+	n, _, ok = a.Match([]byte("123"))
+	if !ok || n != 0 {
+		t.Error("match fail")
+	}
+}
+
+func TestAtMost(t *testing.T) {
+	a := BetweenByte('a', 'z').AtMost(3)
+	n, _, ok := a.Match([]byte("add"))
+	if !ok || n != 3 {
+		t.Error("match fail")
+	}
+	n, _, ok = a.Match([]byte("123"))
+	if !ok || n != 0 {
+		t.Error("match fail")
+	}
+	n, _, ok = a.Match([]byte("flag"))
+	if !ok || n != 3 {
+		t.Error("match fail")
+	}
+}
+
+func TestLoop(t *testing.T) {
+	a := BetweenByte('0', '9').Loop(IfNot('0'))
+	n, _, ok := a.Match([]byte("11101"))
+	if !ok && n != 3 {
+		t.Error("match fail")
+	}
+	n, _, ok = a.Match([]byte("011"))
+	if !ok && n != 1 {
+		t.Error("match fail")
+	}
+	n, _, ok = a.Match([]byte("111"))
+	if !ok && n != 3 {
+		t.Error("match fail")
+	}
+}
+
+func TestOptional(t *testing.T) {
+	a := BetweenByte('a', 'z')
+	b := BetweenByte('0', '9')
+	c := Con(a, Optional(b))
+	n, _, ok := c.Match([]byte("a"))
+	if !ok && n != 1 {
+		t.Error("match fail")
+	}
+	n, _, ok = c.Match([]byte("a9"))
+	if !ok && n != 2 {
+		t.Error("match fail")
+	}
+	n, _, ok = c.Match([]byte("9a"))
+	if ok {
+		t.Error("match should fail")
+	}
+}
+
+func TestComplement(t *testing.T) {
+	//a := Con(BetweenByte('a', 'z'), BetweenByte('a', 'z').Complement())
+	//a.SaveSVG("/tmp/a.svg")
+	//n, _, ok := a.Match([]byte("a"))
+	//if !ok && n != 1 {
+	//	t.Error("match fail")
+	//}
+	//n, _, ok = a.Match([]byte("a9"))
+	//if !ok && n != 2 {
+	//	t.Error("match fail")
+	//}
+	//n, _, ok = a.Match([]byte("aaa"))
+	//println(n)
+	//if ok {
+	//	t.Error("match should fail")
+	//}
+}
+
+func TestExclude(t *testing.T) {
+	a := BetweenByte('a', 'z').Exclude(Char("aeiou"))
+	n, _, ok := a.Match([]byte("b"))
+	if !ok && n != 1 {
+		t.Error("match fail")
+	}
+	n, _, ok = a.Match([]byte("a"))
+	if ok {
+		t.Error("match should fail")
+	}
+	n, _, ok = a.Match([]byte("9a"))
+	if ok {
+		t.Error("match should fail")
+	}
+}
+
+func TestRepeat(t *testing.T) {
+	a := BetweenByte('a', 'z')
+	b := a.Repeat()
+	n, _, ok := b.Match([]byte("b"))
+	if !ok && n != 1 {
+		t.Error("match fail")
+	}
+	n, _, ok = b.Match([]byte("bbb"))
+	if !ok && n != 3 {
+		t.Error("match fail")
+	}
+	n, _, ok = b.Match([]byte(""))
+	if !ok && n != 0 {
+		t.Error("match fail")
+	}
+	n, _, ok = b.Match([]byte("1"))
+	if !ok && n != 0 {
+		t.Error("match fail")
+	}
+
+	b = a.Repeat(3)
+	n, _, ok = b.Match([]byte("bbb"))
+	if !ok && n != 3 {
+		t.Error("match fail")
+	}
+	n, _, ok = b.Match([]byte("bb"))
+	if ok {
+		t.Error("match should fail")
+	}
+	n, _, ok = b.Match([]byte("bbbb"))
+	if !ok && n != 3 {
+		t.Error("match fail")
+	}
+
+	b = a.Repeat(2, 3)
+	n, _, ok = b.Match([]byte("bbb"))
+	if !ok && n != 3 {
+		t.Error("match fail")
+	}
+	n, _, ok = b.Match([]byte("bb3"))
+	if !ok && n != 2 {
+		t.Error("match fail")
+	}
+	n, _, ok = b.Match([]byte("bbbb"))
+	if !ok && n != 3 {
+		t.Error("match fail")
+	}
+}
+
+func TestInvalidPrefix(t *testing.T) {
+	a := BetweenByte('a', 'z')
+	b := Or(a.Repeat(2).As(1), a.Repeat(3).As(2))
+
+	c := b.InvalidPrefix().Minimize()
+	//println(c.String())
+	n, _, ok := c.Match([]byte("x0"))
+	if !ok || n != 2 {
+		t.Error("match fail")
+	}
+	n, _, ok = c.Match([]byte("xx"))
+	if ok {
+		t.Error("match should fail")
+	}
+	n, _, ok = c.Match([]byte("abc"))
+	if ok {
+		t.Error("match should fail")
+	}
 }
